95c202661737bc045a01b5ef4351084343981796
Alex Chaphiv <alex.chaphiv@whitehawk.com>
2024-04-12 15:51:47 -0400

    feat: remove custom generation for `web`

--- web/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup'
-
-export default defineConfig({
-  esbuildOptions(options, _context) {
-    // Preserve directory structure
-    // E.G. `./src-gen/foo/bar.ts` -> `./dist/foo/bar.ts`
-    options.outbase = './src-gen'
-  }
-})
