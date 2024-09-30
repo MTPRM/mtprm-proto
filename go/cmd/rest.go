d4692548716786dd620e576e6ba4822501658e60
Amit Niroula <amit.niroula.5@gmail.com>
2024-09-30 15:26:44 -0400

    feat: grpc proxied

---
+++ go/cmd/rest.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	entities_api "github.com/MTPRM/mtprm-proto-grpc-gateway/generated/mtprm/api/portfolio/beta/resources/entities/v1"
+	entities__0__reports__combined__xlsx "github.com/MTPRM/mtprm-proto-grpc-gateway/generated/mtprm/api/portfolio/beta/resources/entities__0__reports__combined__xlsx/v1"
+	entities__0__reports__summary "github.com/MTPRM/mtprm-proto-grpc-gateway/generated/mtprm/api/portfolio/beta/resources/entities__0__reports__summary/v1"
+	reports__combined__zip "github.com/MTPRM/mtprm-proto-grpc-gateway/generated/mtprm/api/portfolio/beta/resources/reports__combined__zip/v1"
+)
+
+func main() {
+	port := ":8080"
+
+	log.Println("Starting listening on port:", port)
+	mux := runtime.NewServeMux()
+
+	grpcServerEndpoint := os.Args[1]
+	// grpc.WithTransportCredentials(insecure.NewCredentials())
+	// opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+
+	// Set up a connection to the order server.
+	conn, err := grpc.Dial(grpcServerEndpoint, grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{})))
+	if err != nil {
+		log.Fatalf("could not connect to order service: %v", err)
+	}
+	defer conn.Close()
+
+	err = entities_api.RegisterServiceHandler(context.Background(), mux, conn)
+	if err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+	err = entities__0__reports__combined__xlsx.RegisterServiceHandler(context.Background(), mux, conn)
+	if err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+	err = entities__0__reports__summary.RegisterServiceHandler(context.Background(), mux, conn)
+	if err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+	err = reports__combined__zip.RegisterServiceHandler(context.Background(), mux, conn)
+	if err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+	err = http.ListenAndServe(port, mux)
+	if err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
---
+++ go/go.mod
@@ -0,0 +1,17 @@
+module github.com/MTPRM/mtprm-proto-grpc-gateway
+
+go 1.23.1
+
+require (
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.22.0
+	google.golang.org/grpc v1.67.0
+	google.golang.org/protobuf v1.34.2
+
+)
+
+require (
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+	google.golang.org/genproto v0.0.0-20220118154757-00ab72f36ad5 // indirect
+)
