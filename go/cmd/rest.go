4d946a7fecb286e9f9d333226ee402285ab407c5
Alex Chaphiv <alex.chaphiv@whitehawk.com>
2024-10-01 02:26:16 -0400

    fix(go): send `application/x-ndjson` for streaming response

--- go/cmd/rest.go
+++ go/cmd/rest.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 
@@ -19,11 +20,21 @@ import (
 	reports__combined__zip "github.com/mtprm/mtprm-proto-grpc-gateway/generated/mtprm/api/portfolio/beta/resources/reports__combined__zip/v1"
 )
 
-func main() {
-	port := ":8080"
+const ndJsonMimeType = "application/x-ndjson"
 
-	log.Println("Starting listening on port:", port)
-	mux := runtime.NewServeMux()
+type NdJsonMarshaller struct {
+	runtime.JSONPb
+}
+
+func (m *NdJsonMarshaller) ContentType(v interface{}) string {
+	return ndJsonMimeType
+}
+
+func main() {
+	mux := runtime.NewServeMux(
+		// This only gets called if `Accept` header matches
+		runtime.WithMarshalerOption(ndJsonMimeType, &NdJsonMarshaller{}),
+	)
 
 	grpcServerEndpoint := os.Args[1]
 	// grpc.WithTransportCredentials(insecure.NewCredentials())
@@ -61,10 +72,25 @@ func main() {
 		})
 	}
 
-	err = http.ListenAndServe(port, mux)
+	supportNdjson := func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// This has to be set so that the marshaller sees it
+			if strings.HasSuffix(r.URL.Path, ".Service/List") {
+				r.Header.Set("Accept", ndJsonMimeType)
+			}
+
+			h.ServeHTTP(w, r)
+		})
+	}
+
+	port := ":8080"
+
+	err = http.ListenAndServe(port, supportNdjson(mux))
 	if err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+
+	log.Println("Listening on port:", port)
 
 	defer conn.Close()
 }
